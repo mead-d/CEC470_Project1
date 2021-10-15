efadebbfbbe197f2da6168323b0fb8d4b1e86153
mead-d <meadd@my.erau.edu>
2021-10-14 20:27:22 -0400

    Adds file reader

--- TwoStageInstructionDecoder.c
+++ TwoStageInstructionDecoder.c
@@ -1,7 +1,7 @@
 /*-----------------------------------------------
  Submitted By: Dillon Mead, Benjamin Ranew, Michael Hall
  Homework Number: Project 1
- Credit to: N/A
+ Credit to: Aaron Van De Brook for memory file reader.
  
 
  Submitted On: 11 November 2021
@@ -11,11 +11,10 @@
  of this program is primarily my own work.
 ------------------------------------------------*/
 #include <stdint.h>
+#include <stdio.h>
 #define HALT_OPCODE 0x19
 
-/**
- * Registers simulated as global variables
- */
+// Registers simulated as global variables
 
 // 16 bit Program Counter. Address of next instruction
 uint16_t pc = 0;
@@ -32,10 +31,9 @@ uint8_t acc = 0;
 // memory array
 unsigned char memory[65536];
 
-
 /**
  * @param No input.
- * @return No returns.
+ * @return No return.
  * Uses program counter as index of memory array. Stores instruction from memory into instruction register.
  * Increases program counter to point at the next instruction.
  */
@@ -45,7 +43,7 @@ void fetchNextInstruction(void){
 
 /**
  * @param No input.
- * @return No returns.
+ * @return No return.
  * Examines ir and determines execution opertion. Performs operation on registers and memory.
  */
 void executeInstruction(void){
@@ -53,6 +51,20 @@ void executeInstruction(void){
 }
 
 int main(int argc, char* argv[]){
+    // File reader transfers info from memory file to memory array.
+    if(argc != 3) {
+        fprintf(stderr, "Please provide an input file, such as: .\%s [mem_in] [mem_out]\n", argv[0]);
+        return -1;
+    }
+
+    FILE *mem_out, *mem_in = fopen(argv[1], "r");
+    uint8_t *mem_ptr = memory;
+
+    while(!feof(mem_in)){
+        fscanf(mem_in, "%x", mem_ptr++);
+    }
+    fclose(mem_in);
+
     // Execution loop. Continue fetching and executing until program counter
     // points to a HALT instruction.
     while(memory[pc] != HALT_OPCODE){
