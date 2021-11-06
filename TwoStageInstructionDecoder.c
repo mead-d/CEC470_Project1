dd50e3011fbf4740a45e6521789d8a20fa135a81
btranew10 <84579990+btranew10@users.noreply.github.com>
2021-11-06 09:16:10 -0400

    Update to Execute Function

--- TwoStageInstructionDecoder.c
+++ TwoStageInstructionDecoder.c
@@ -241,8 +241,225 @@ else if((ir & 0xf8) == 0x10){ //This initiates branching statements
  */
 //You could potentially use getfetch code, considering it DOSE ulitizes roughly the same format
 void executeInstruction(void){
+//Ranew Start
+    int address;
 
-}
+	if ((ir & 0x80) == 0x80){ // Mathematical or Logical Operation
+		// Destination and source values
+		int destination;
+		int source;
+
+		switch(ir & 0x0c){ // Destination
+			case 0x0:
+				destination = memory[mar]; // Indirect (mar used as a pointer)
+				break;
+			case 0x4:
+				destination = acc; // Accumulator acc
+				break;
+			case 0x8:
+				destination = mar; // Address register mar
+				break;
+			case 0xc:
+				destination = memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])]; // Memory
+				break;
+		}
+
+		switch(ir & 0x03){ // Source
+			case 0x0:
+				source = memory[mar]; // Indirect (mar used as a pointer)
+				break;
+			case 0x1:
+				source = acc; // Accumulator acc
+				break;
+			case 0x2:
+				if((ir & 0x0c) == 0x8) // Constant
+					source = (memory[pc - 2] << 8) + memory[pc -1];
+				else
+					source = memory[pc - 1];
+				break;
+			case 0x3:
+				if((ir & 0x0c) == 0x8){ // Memory
+					address = ((memory[old_pc + 1] << 8) + memory[old_pc + 2]);
+					source = (memory[address] << 8) + memory[address + 1];
+				}
+				else
+					source = memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])];
+				break;
+		}
+
+		switch (ir & 0x70){ // Checking the function
+			case 0x00: // AND
+				destination &= source;
+				break;
+			case 0x10: // OR
+				destination |= source;
+				break;
+			case 0x20: // XOR
+				destination ^= source;
+				break;
+			case 0x30: // ADD
+				destination += source;
+				break;
+			case 0x40: // SUB
+				destination -= source;
+				break;
+			case 0x50: // INC
+				destination++;
+				break;
+			case 0x60: // DEC
+				destination--;
+				break;
+			case 0x70: // NOT
+				destination = !destination;
+			break;
+			default:
+				break;
+		}
+		// Store destination
+		switch(ir & 0x0c){ // Isolate destination id
+			case 0x0:
+				memory[mar] = destination & 0xff; // Indirect (mar used as a pointer)
+				break;
+			case 0x4:
+				acc = destination & 0xff; // Accumulator acc
+				break;
+			case 0x8:
+				mar = destination & 0xffff; // Address register mar
+				break;
+			case 0xc:
+				memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])] = destination & 0xff; // Memory
+				break;
+		}
+	}
+
+	else if ((ir & 0xf0) == 0){ // Store/Load function
+		if ((ir & 0x08) == 0){ // Store
+			if ((ir & 0x04) == 0){ // Storing from Register = Accumulator acc
+				switch(ir & 0x03){
+					case 0:
+						memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])] = acc; // Storing with Method = Operand is used as address
+						break;
+					case 1: // Operand is used as a Constant
+						break;
+					case 2:
+						memory[mar] = acc; // Storing with Method = Indirect (mar used as a pointer)
+						break;
+					default:
+						break;
+				}
+			}
+			else{ // Storing from Register = Index register mar
+				switch(ir & 0x03){
+					case 0:
+						memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])] = (mar >> 8) & 0xff; // Storing with Method = Operand is used as address
+						memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2]) + 1] = mar & 0xff;
+						break;
+					case 1: // Operand is used as a Constant
+						break;
+					case 2:
+						memory[mar] = (mar >> 8) & 0xff; // Storing with Method = Indirect (mar used as a pointer)
+						memory[mar + 1] = mar & 0xff;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		else{ // Load
+			if ((ir & 0x04) == 0){ // Loading into register Accumulator acc
+				switch(ir & 0x03){
+					case 0:
+						acc = memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])]; // Method = Operand is used as address
+						break;
+					case 1:
+						acc = memory[old_pc + 1]; // Method = Operand is used as a constant
+						break;
+					case 2:
+						acc = memory[mar]; // Method = Indirect (mar used as a pointer)
+						break;
+					default:
+						break;
+				}
+			}
+			else{ // Loading into mar
+				int oldMAR = mar;
+
+				switch(ir & 0x03){
+					case 0: // Method = Operand is used as address
+						mar = memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2])];
+						mar <<=8;
+						mar += memory[((memory[old_pc + 1] << 8) + memory[old_pc + 2]) + 1];
+						break;
+					case 1: // Method = Operand is used as a constant
+						mar = memory[old_pc + 1];
+						mar <<= 8;
+						mar += memory[old_pc + 2];
+						break;
+					case 2: // Method = Indirect (mar used as pointer)
+						mar = memory[oldMAR];
+						mar <<= 8;
+						mar += memory[oldMAR + 1];
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+
+	else if ((ir & 0xF8) == 0x10){ // Branches
+		address = (memory[old_pc + 1] << 8) + memory[old_pc + 2];
+
+		switch (ir & 0x07){
+			case 0: // BRA (Unconditional branch/branch always)
+				pc = address;
+				break;
+			case 1: // BRZ (Branch if acc = 0)
+				if (acc == 0)
+					pc = address;
+				break;
+			case 2: // BNE (Branch if acc != 0)
+				if (acc != 0)
+					pc = address;
+				break;
+			case 3: // BLT (Branch if acc < 0)
+				if ((acc & 0x80) != 0)
+					pc = address;
+				break;
+			case 4: // BLE (Branch if acc <= 0)
+				if (((acc & 0x80) != 0) || (acc == 0))
+					pc = address;
+				break;
+			case 5: // BGT (Branch if acc > 0)
+				if (((acc & 0x80) == 0) && (acc != 0))
+					pc = address;
+				break;
+			case 6: // BGE (Branch if acc >= 0)
+				if ((acc & 0x80) == 0)
+					pc = address;
+				break;
+			default:
+				break;
+		}
+	}
+
+	else{ // All else is either a "No Operation", "Halt" or and illegal opcode.
+		if(ir == 0x18) // NOP "No Operation"
+		{
+
+		}
+
+		else if(ir == 0x19) // Halt
+		{
+
+		}
+
+		else // Illegal Operation
+		{
+
+		}
+	}
+} // Ranew End
 
 int main(int argc, char* argv[]){
     // File reader transfers info from memory file to memory array.
